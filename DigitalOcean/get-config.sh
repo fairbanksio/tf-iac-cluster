da36763b7390674444a76daf5647f3f72802d47e
Jon Fairbanks <fairplay89@gmail.com>
2020-07-28 17:25:10 -0700

    Initial commit

---
+++ DigitalOcean/get-config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CLUSTER_ID=$(terraform output cluster-id)
+
+echo "Getting k8s config for $CLUSTER_ID"
+
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${TF_VAR_do_token}" "https://api.digitalocean.com/v2/kubernetes/clusters/$CLUSTER_ID/kubeconfig" > config
+
