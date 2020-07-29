dd1a6eac59d93ad17be364cb727ee7b9138b1d1b
Jon Fairbanks <fairplay89@gmail.com>
2020-07-28 19:14:01 -0700

    Fix formatting

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # The configuration for the `remote` backend.
 terraform {
-    backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "Fairbanks-io"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Fairbanks-io"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "k8s-prod-us-sfo"
-        }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "k8s-prod-us-sfo"
     }
+  }
 }
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
-    triggers = {
-        value = "A example resource that does nothing!"
-    }
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
 }
