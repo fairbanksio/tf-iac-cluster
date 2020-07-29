7db982a296ea0b376a38e96c93c9392934b31bab
Jon Fairbanks <fairplay89@gmail.com>
2020-07-28 17:38:30 -0700

    Move config after downloading

--- DigitalOcean/get-config.sh
+++ DigitalOcean/get-config.sh
@@ -6,3 +6,4 @@ echo "Getting k8s config for $CLUSTER_ID"
 
 curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${TF_VAR_do_token}" "https://api.digitalocean.com/v2/kubernetes/clusters/$CLUSTER_ID/kubeconfig" > config
 
+mv config ~/.kube/config
