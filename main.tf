c3119eca7ba8ccf08f1f656b4f5709a30cd164a9
Jon Fairbanks <fairplay89@gmail.com>
2020-07-28 20:04:55 -0700

    Var names

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-variable "do_token" {}
-variable "do_cluster_name" {}
-variable "dd_api_key" {}
+variable "DO_TOKEN" {}
+variable "DO_CLUSTER_NAME" {}
+variable "DD_API_KEY" {}
 
 # The configuration for the `remote` backend.
 terraform {
