14aef174e19f7d3d842f768a8a6bbb3abad83bc3
Jon Fairbanks <fairplay89@gmail.com>
2020-08-21 20:13:26 -0700

    Nope

--- resources.tf
+++ resources.tf
@@ -95,10 +95,6 @@ resource "helm_release" "ingress" {
     name  = "controller.config.proxy-read-timeout"
     value = "60s"
   }
-  set {
-    name  = "controller.config.enable-real-ip"
-    value = ""true""
-  }
 }
 
 ## MongoDB
