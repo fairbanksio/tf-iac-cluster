9b3f4721c75be4be0b7449da473bdff499f4f0a8
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-05 17:39:21 -0500

    Remove bad configs and add pretty-default-backend

--- resources.tf
+++ resources.tf
@@ -2,7 +2,7 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
   name         = var.do_cluster_name
   region       = "sfo2"
-  auto_upgrade = true
+  auto_upgrade = false
   version      = "1.18.8-do.0"
 
   node_pool {
@@ -12,10 +12,10 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   }
 }
 
-resource "digitalocean_spaces_bucket" "static-assets" {
-  name   = var.do_space_name
-  region = "sfo2"
-}
+#resource "digitalocean_spaces_bucket" "static-assets" {
+#  name   = var.do_space_name
+#  region = "sfo2"
+#}
 
 ## kube-state-metrics
 
@@ -76,41 +76,20 @@ resource "helm_release" "ingress" {
     value = "nginx-ingress-controller"
   }
   set {
-    name  = "controller.service.externalTrafficPolicy"
-    value = "Local"
+    name  = "defaultBackend.enabled"
+    value = "false"
   }
   set {
-    name  = "controller.config.proxy-body-size"
-    value = "250m"
+    name  = "controller.defaultBackendService"
+    value = "default/pretty-default-backend"
   }
-  set {
-    name  = "controller.config.client-max-body-size"
-    value = "250m"
-  }
-  set {
-    name  = "controller.config.proxy-connect-timeout"
-    value = "60s"
-  }
-  set {
-    name  = "controller.config.proxy-read-timeout"
-    value = "60s"
-  }
-  set {
-    name  = "controller.config.proxy-read-timeout"
-    value = "60s"
-  }
-  set {
-    name  = "defaultBackend.image.repository"
-    value = "fairbanksio/pretty-default-backend"
-  }
-  set {
-    name  = "defaultBackend.image.tag"
-    value = "latest"
-  }
-  set {
-    name  = "defaultBackend.port"
-    value = "80"
-  }
+}
+
+resource "helm_release" "pretty-default-backend" {
+  name       = "pretty-default-backend"
+  repository = "https://h.cfcr.io/fairbanks.io/default"
+  chart      = "pretty-default-backend"
+  namespace  = "default"
 }
 
 ## MongoDB
