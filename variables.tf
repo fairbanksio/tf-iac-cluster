0bb40d366dad20d08a4f2f0223e1e3c55c2589cb
Jon Fairbanks <fairplay89@gmail.com>
2020-10-11 02:50:01 -0700

    Formatting

--- variables.tf
+++ variables.tf
@@ -1,20 +1,11 @@
-# Digital Ocean
 variable "do_token" {}
 variable "do_cluster_name" {}
-
-# Digital Ocean Spaces
 variable "do_access_id" {}
 variable "do_secret_key" {}
 variable "do_space_name" {}
-
-# DataDog
 variable "dd_api_key" {}
-
-# CloudFlare
 variable "cloudflare_email" {}
 variable "cloudflare_api_key" {}
 variable "cloudflare_zone_id" {}
 variable "cloudflare_zone_id_fairbanks" {}
-
-# Grafana
 variable "grafana_password" {}
