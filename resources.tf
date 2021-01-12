1bc927f480b8459a85a4d1920a2c3af87da1db3a
mgmtadmin <brandon.sorgdrager@gmail.com>
2021-01-12 17:17:07 -0600

    feat(argo): fix comments

--- resources.tf
+++ resources.tf
@@ -158,7 +158,7 @@ resource "helm_release" "pretty-default-backend" {
 }
 
 ## Argo CD
-/* ## Monitoring
+/*
 resource "kubernetes_namespace" "argo-cd" {
   metadata {
     name = "argo-cd"
@@ -177,7 +177,7 @@ resource "helm_release" "argo-cd" {
 }
 
 
-/* ## Monitoring
+## Monitoring
 
 resource "kubernetes_namespace" "monitoring" {
   metadata {
