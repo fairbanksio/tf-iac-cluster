12ce3d2da3d944ba126344f4848644dfdc91f57f
mgmtadmin <brandon.sorgdrager@gmail.com>
2021-03-30 13:59:07 -0500

    fix(scaling): change node size

--- resources.tf
+++ resources.tf
@@ -3,14 +3,14 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   name         = var.do_cluster_name
   region       = "sfo2"
   auto_upgrade = false
-  version      = "1.18.10-do.3"
+  version      = "1.20.2-do.0"
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
+    size       = "s-2vcpu-4gb"
     auto_scale = true
     min_nodes  = 3
-    max_nodes  = 4
+    max_nodes  = 3
   }
 }
 
