dbaffd05e4ef0ee492bce7b2feb3f76f1cc9ee5e
bsord <brandon.sorgdrager@gmail.com>
2021-11-17 19:16:19 -0600

    remove apostrophe

--- resources.tf
+++ resources.tf
@@ -19,7 +19,7 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
 
 ## FluxCD
 locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg='"
+  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
 }
 
 resource "kubernetes_namespace" "flux" {
