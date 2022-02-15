83a34aad4e0f73155df077a7baf39a32280258a0
Jon Fairbanks <fairplay89@gmail.com>
2022-02-15 15:29:40 -0800

    Set auto-upgrade schedule

--- resources.tf
+++ resources.tf
@@ -10,6 +10,11 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   auto_upgrade  = true
   surge_upgrade = true
   version       = data.digitalocean_kubernetes_versions.latest.latest_version
+  
+  maintenance_policy {
+    start_time  = "13:00"
+    day         = "sunday"
+  }
 
   node_pool {
     name       = "worker-pool"
