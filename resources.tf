b87a452fc883d3a65b8d388e49e324cf86da1568
Jon Fairbanks <fairplay89@gmail.com>
2022-03-05 00:36:31 -0800

    Remix

--- resources.tf
+++ resources.tf
@@ -100,7 +100,7 @@ resource "cloudflare_record" "f5" {
   provider = cloudflare.cloudflare-fairbanks
   zone_id  = var.cloudflare_zone_id_fairbanks_dev
   name     = "f5"
-  value    = data.kubernetes_service.nginx-ingress-controller.load_balancer_ingress.0.ip
+  value    = data.digitalocean_kubernetes_cluster.k8s.ipv4_address
   type     = "A"
 }
 
