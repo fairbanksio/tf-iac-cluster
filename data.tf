bbd715755daf071a01bf410dea9e1df10494aed1
bsord <brandon.sorgdrager@gmail.com>
2022-10-23 14:22:03 -0500

    Remove unused ingress and associated pretty-default-backend

--- data.tf
+++ data.tf
@@ -1,10 +1,3 @@
 data "digitalocean_kubernetes_cluster" "k8s" {
   name = "fairbanks-io"
 }
-
-data "kubernetes_service" "nginx-ingress-controller" {
-  metadata {
-    name = "ingress-ingress-nginx-controller"
-  }
-  depends_on = [helm_release.ingress]
-}  
